84e61753263984cc78dc158ba42adcf9cebca9da
Sone <sthaya@gmail.com>
2020-04-24 08:26:10 -0700

    answered self study questions, created home and pizza components

---
+++ src/components/Congrats.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Congrats() {
+  return (
+    <h1>Congrats Your pizza is on its way</h1>
+  )
+}
+
+export default Congrats
---
+++ src/components/Pizza.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Styles } from './Styles'
+import { Link } from 'react-router-dom';
+
+function Pizza(props) {
+  const {
+    values,
+    onInputChange,
+    onCheckboxChange,
+    onSubmit,
+    errors,
+  } = props
+
+
+  return (
+
+    <Styles>
+
+    <form>
+      <div>
+        <h1>Pizza Page</h1>
+      </div>
+
+      <div className='errors'></div>
+
+      <label>Name:&nbsp;
+      <p className="errors">{errors.name}</p>
+      <input
+          value={values.name}
+          onChange={onInputChange}
+          name='name'
+          type='text'
+        /></label>
+      
+        <label>Pizza Size:&nbsp;
+        <p className="errors">{errors.size}</p>
+        <select
+            value={values.size}
+            onChange={onInputChange}
+            name='size'
+          >
+            <option defaultValue=''>Please choose</option>
+            <option value='small'>Small</option>
+            <option value='medium'>Medium</option>
+            <option value='large'>Large</option>
+        </select></label>
+      
+        <label><input
+          checked={values.toppings.cheese}
+          onChange={onCheckboxChange}
+          name='cheese'
+          type="checkbox" /> Cheese</label>
+        <label><input
+          checked={values.toppings.pepperoni}
+          onChange={onCheckboxChange}
+          name='pepperoni'
+          type="checkbox" /> Pepperoni</label>
+        <label><input
+          checked={values.toppings.pineapple}
+          onChange={onCheckboxChange}
+          name='pineapple'
+          type="checkbox" /> Pineapple</label>
+        <label><input
+          checked={values.toppings.bacon}
+          onChange={onCheckboxChange}
+          name='bacon'
+          type="checkbox" /> Bacon</label>
+      
+          <label><input
+            value={values.special}
+            onChange={onInputChange}
+            name='special'
+            type="textarea" /> Special Instructions</label>
+        
+          
+      
+            <button onClick={onSubmit}><Link to='/congrats'>Submit Order</Link></button>
+      
+      </form>
+        
+    </Styles>
+  )
+}
+
+export default Pizza
